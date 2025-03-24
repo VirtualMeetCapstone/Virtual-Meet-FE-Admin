130cc52140d91a51c2d81fe57fbbef69b5b8b583
duchadev <duchadev145@gmail.com>
2025-03-24 22:26:47 +0700

    update user/room/post management UI

---
+++ src/app/meeting-report/meeting-report.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-report',
+  imports: [],
+  templateUrl: './meeting-report.component.html',
+  styleUrl: './meeting-report.component.scss'
+})
+export class MeetingReportComponent {
+
+}
---
+++ src/app/post-report/post-report.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-report',
+  imports: [],
+  templateUrl: './post-report.component.html',
+  styleUrl: './post-report.component.scss'
+})
+export class PostReportComponent {
+
+}
