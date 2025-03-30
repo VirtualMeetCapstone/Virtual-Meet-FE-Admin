9fe85d923a80ff4f365220678bf526e8452828a1
duchadev <duchadev145@gmail.com>
2025-03-30 19:29:24 +0700

    update post detail

--- src/app/model/post.ts
+++ src/app/model/post.ts
@@ -3,20 +3,27 @@ export interface Media {
     type: number;
     thumbnailUrl: string | null;
   }
-  
+
   export interface Comment {
     // Thêm các thuộc tính của comment nếu có, hiện tại đang là mảng rỗng
   }
-  
+
   export interface Post {
     id: string;
     content: string;
     userId: string;
-    comments: Comment[]; 
+    user: {
+      name: string;
+      picture: {
+        url: string;
+        type: number;
+        thumbnailUrl: string | null;
+      };
+    };
+    comments: Comment[];
     medias: Media[];
     privacy: number;
     countReaction: number;
     createTime: number;
     lastModifyTime: number;
   }
-  
