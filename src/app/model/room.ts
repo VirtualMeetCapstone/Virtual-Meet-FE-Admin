96d42f376599259f08d2e8b99aa27524a1491698
duchadev <duchadev145@gmail.com>
2025-04-13 17:17:28 +0700

    format datetime, fix layout, report row

--- src/app/model/room.ts
+++ src/app/model/room.ts
@@ -1,25 +1,27 @@
 export interface Room {
-    id: string;
-    ownerId: string;
-    owner: {
-      name: string;
-      picture: {
-        url: string;
-        type: number;
-        thumbnailUrl: string | null;
-      };
-    };
-    topic: string;
-    description: string;
-    maximumMembers: number;
-    medias: Array<{
+  id: string;
+  ownerId: string;
+  owner: {
+    name: string;
+    picture: {
       url: string;
       type: number;
       thumbnailUrl: string | null;
-    }>;
-    status: string | null;
-    members: Array<any>;  // Type of members array not provided, using any.
-    createTime: number;  
-    lastModifyTime: number; 
-  }
-  
+    };
+  };
+  topic: string;
+  description: string;
+  maximumMembers: number;
+  medias: Array<{
+    url: string;
+    type: number;
+    thumbnailUrl: string | null;
+  }>;
+  status: string | null;
+  members: Array<any>;  // Type of members array not provided, using any.
+  createTime: number;
+  lastModifyTime: number;
+  isReported: boolean;
+  formattedCreateTime?: string;
+
+}
