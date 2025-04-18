d9ea9585cc08dc24233b9f297ec9a5fae9ed1f2c
duchadev <duchadev145@gmail.com>
2025-04-18 23:37:46 +0700

    logo configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import { HeaderAdminComponent } from "./header-admin/header-admin.component";
 import { SidebarAdminComponent } from "./sidebar-admin/sidebar-admin.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { FooterComponent } from "./footer/footer.component";
 import { CommonModule } from '@angular/common';
-import { provideHttpClient } from '@angular/common/http'; // Import provideHttpClient
+import { provideHttpClient } from '@angular/common/http';
+import {LogoServiceService} from './Service/logo-service/logo-service.service'; // Import provideHttpClient
 
 @Component({
   selector: 'app-root',
@@ -15,9 +16,10 @@ import { provideHttpClient } from '@angular/common/http'; // Import provideHttpC
   styleUrls: ['./app.component.scss'], // Corrected from styleUrl to styleUrls
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Virtual-Meet-FE-Admin-main';
-  constructor(private router: Router) {
+  logoUrl = 'assets/logo.png';
+  constructor(private router: Router, private logoService: LogoServiceService) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         const body = document.body;
@@ -28,6 +30,19 @@ export class AppComponent {
           body.classList.add('other-page');
           body.classList.remove('dashboard-page');
         }
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.logoService.getLogo().subscribe({
+      next: (res) => {
+        if (res?.media?.url) {
+          this.logoUrl = res.media.url;
+        }
+      },
+      error: () => {
+        console.error('Failed to load logo');
       }
     });
   }
