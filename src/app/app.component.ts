c7d34104e94ec45208e69ca975217bd877197304
duchadev <duchadev145@gmail.com>
2025-04-23 01:56:06 +0700

    list user reported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent implements OnInit {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         const body = document.body;
-        if (event.urlAfterRedirects === '/' || event.url === '/dashboard' || event.url.startsWith('/post-detail/')) {
+        if (event.urlAfterRedirects === '/' || event.url === '/dashboard' || event.url.startsWith('/post-detail/') || event.url === '/report-list') {
           body.classList.add('dashboard-page');
           body.classList.remove('other-page');
         } else {
